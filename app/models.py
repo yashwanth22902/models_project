c2b02bd34ad56b73a8cb96278c53296d81f615ee
yaswanth <yashwanthreddyramireddy@gmail.com>
2023-12-08 17:14:37 +0530

    first commit

--- app/models.py
+++ app/models.py
@@ -4,11 +4,18 @@ from django.db import models
 
 class Topic(models.Model):
     topic_name=models.CharField(max_length=100,primary_key=True)
+    def __str__(self):
+        return self.topic_name
 class Webpage(models.Model):
-    topic_name=models.ForeignKey(Topic,on_delete=models.CASCADE)
+    tpic_name=models.ForeignKey(Topic,on_delete=models.CASCADE)
     name=models.CharField(max_length=100)
     url=models.URLField()
+    def __str__(self):
+        return self.name
 class AcessRecord(models.Model):
     name=models.ForeignKey(Webpage,on_delete=models.CASCADE)
     date=models.DateField()
     author=models.CharField(max_length=100)
+    def __str__(self):
+        return self.author
+
